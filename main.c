d994dfc8b9fbc30058433aaf4138e2ab86eb8901
gejiahao <sigmax3128@qq.com>
2024-06-04 21:26:41 +0800

    fisrt test

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
+#define ARRAY_SIZE 10
 #include "test_header.h"
 int main() {
     printf("Hello, World!\n");
-    printf("%lf",MY_PI);
+    printf("%lf\n",MY_PI);
+    int i;
+    for (i = 0; i < ARRAY_SIZE; i++)
+    {
+        printf("i::%d\n",i);
+        if (i > 5.09)
+        {
+            printf("I__I\n");
+        }
+    }
     return 0;
 }
